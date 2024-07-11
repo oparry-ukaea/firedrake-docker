fa22424c675d40f68f42b2221c676e21dc656b9d
Owen Parry <owen.parry@ukaea.uk>
2024-07-11 14:28:04 +0100

    Script to generate a singularity image.

---
+++ build.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for cmd in "docker" "singularity"
+do
+    if ! command -v "$cmd" &> /dev/null
+    then
+        echo "$cmd command not on path or doesn't exist"
+        exit 1
+    fi
+done
+
+# Build the docker images
+docker compose build
+
+# Convert to sandbox singularity
+singularity build --force \
+   --sandbox ./firedrake-singularity \
+   docker-daemon://firedrake:latest
+
+# Build singularity imsage
+singularity build --force \
+firedrake.sif \
+./firedrake-singularity
